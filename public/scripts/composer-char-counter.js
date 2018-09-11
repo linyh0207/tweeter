174fe818f0bc2a3849db27398a727f830c9ce664
Angela <liny0207@gmail.com>
2018-09-11 23:34:29 +0000

    implement the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(function() {
+   $('textarea').on("keyup", function (event){
+        var maxWordLimit = 140;
+        var length = this.value.length;
+        var counter = $(this).siblings('span.counter');
+        counter.text(maxWordLimit - length);
+        if (length > maxWordLimit){
+        counter.addClass('red')
+       } else {
+        counter.removeClass('red')
+       }
+   })
+  });
+
