38215c6faf1ca845a45b02f6d4b9ef251fd43812
Angela <liny0207@gmail.com>
2018-09-13 21:50:15 +0000

    updated getTweet function with MongoDB

--- server/index.js
+++ server/index.js
@@ -11,7 +11,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // The in-memory database of tweets. It's a basic object with an array in it.
-const db = require("./lib/in-memory-db");
+const MongoClient = require('mongodb').MongoClient;
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+// const db = db.collection('tweets').find();
+MongoClient.connect(MONGODB_URI, (err, db) => {
+  if (err) throw err;
+
+  function getTweets(callback) {
+    db.collection("tweets").find().toArray(callback);
+  }
 
 // The `data-helpers` module provides an interface to the database of tweets.
 // This simple interface layer has a big benefit: we could switch out the
@@ -32,3 +40,7 @@ app.use("/tweets", tweetsRoutes);
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
+
+});
+
+
